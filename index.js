e01e688582ccda740853621bfe1a316eff60937b
Sarah <sarahlchavez@gmail.com>
2021-09-08 06:16:00 -0700

    commit tree d00898648c2c0c0753e87755f99f76dcd4078128

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+// define a driver variable and assign it to an object
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(object, key, value){
+    const newDriver = { ...object};
+    newDriver[key] = value;
+    return newDriver;
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(object, key, value) {
+    driver[key] = value;
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+const newerDriver = Object.assign({}, driver);
+delete newerDriver[key];
+return newerDriver;
+}
+
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
